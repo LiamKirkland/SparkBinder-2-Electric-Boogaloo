835f45f44dd0fd3103c8452b6b3f78628db3ef53
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-29 08:47:59 -0400

    Update constraints and add .env to ensure server and app run on different ports

--- src/constraints.js
+++ src/constraints.js
@@ -1,3 +1,3 @@
 export const scryURL = ""
-export const collURL = "http://localhost:4000/collection"
-export const auditURL = "http://localhost:4000/audits"
+export const collURL = "http://localhost:3000/collection"
+export const auditURL = "http://localhost:3000/audits"
