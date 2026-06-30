0681f355a00854997cf387ac668a31cf12483e6c
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-30 14:07:28 -0400

    Fix indentation

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -72,8 +72,8 @@ export default function Search() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-      ...selectedCard,
-      ...formData,
+          ...selectedCard,
+          ...formData,
     }),
       })
       
