045a32f7e3f200cb8cbe9acb8e6415351dabe290
LiamKirkland <liam.b.kirkland@gmail.com>
2026-07-01 12:57:39 -0400

    Setup deployment stuff

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const scryURL = "https://api.scryfall.com/cards"
-export const collURL = "http://localhost:3000/collection"
-export const auditURL = "http://localhost:3000/audits"
+export const collURL = `${process.env.REACT_APP_API_URL}/collection` || "http://localhost:3000/collection"
+export const auditURL = `${process.env.REACT_APP_API_URL}/audits` || "http://localhost:3000/audits"
